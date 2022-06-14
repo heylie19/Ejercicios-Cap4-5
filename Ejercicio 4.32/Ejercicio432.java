f07b9247b7068c50a3f1411b55184621a5ec2161
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-06-13 20:33:42 -0600

    Add files via upload

---
+++ Ejercicio 4.32/Ejercicio432.java
@@ -0,0 +1,17 @@
+//(Patrón de asteriscos en forma de tablero de damas) 
+
+public class Ejercicio432 
+{
+    public static void main(String[] args)
+    {
+        String linea[]=new String[4];
+        
+        System.out.println("Tablero");
+
+        for(int i=0; i<linea.length; i++ )
+        {
+            System.out.println("* * * * * * * *");
+            System.out.println(" * * * * * * * *");
+        }
+    }
+}
